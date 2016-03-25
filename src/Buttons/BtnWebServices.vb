86ced3514f4b08fd51d0a0732dfbc2552b842d02
Lesley <lbross@pdx.edu>
2016-03-25 16:09:11 -0700

    Troubleshooting DNS image services problem

--- src/Buttons/BtnWebServices.vb
+++ src/Buttons/BtnWebServices.vb
@@ -53,13 +53,26 @@ Public Class BtnWebServices
         Dim AGSConnectionFactory As IAGSServerConnectionFactory = New AGSServerConnectionFactory
         Dim connectionProps As IPropertySet = New PropertySet
         Dim AGSConnection As IAGSServerConnection = Nothing
+        'This works
         Dim fullPath As String = "http://basins.geog.pdx.edu/arcgis/services/BAGIS_GISLayers/DEM_westus_bc_30m/ImageServer"
         Dim layerExists As Boolean = BA_File_ExistsImageServer(fullPath)
-        fullPath = "http://basins.geog.pdx.edu/arcgis/services/BAGIS_PRISM/Prism_Precipitation_01/ImageServer"
-        layerExists = BA_File_ExistsImageServer(fullPath)
+        'This doesn't (DNS alias)
         fullPath = "http://webservices.geog.pdx.edu/arcgis/services/BAGIS_GISLayers/DEM_westus_bc_30m/ImageServer"
         layerExists = BA_File_ExistsImageServer(fullPath)
     End Sub
+
+    Private Function File_ExistsImageServer(ByVal imageUrl As String) As Boolean
+        Dim isLayer As IImageServerLayer = New ImageServerLayerClass
+        Dim imageRaster As IRaster = Nothing
+        Try
+            isLayer.Initialize(imageUrl)
+            imageRaster = isLayer.Raster
+            Return True
+        Catch ex As Exception
+            ' An exception was thrown while trying to open the dataset, return false
+            Return False
+        End Try
+    End Function
 
 End Class
 
